8ffdb98fd12c1807c38be07fb64ea24f291f1b56
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-04 23:51:54 +0100

    Initial commit: Datasoup!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datasoup::Application.config.secret_token = 'ab5c3ca752da2a67bdaaae7075270f247924764513531e58059949ff5dc59a507f5b97249f1ae0bc17a5445364e1076c5d7e376600e3e1bff79094ff87d42fa1'
