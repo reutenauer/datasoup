58e4eeb47b75d4dcc779ce1707fe23a72a0232bf
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-05 18:08:05 +0100

    Quickfix.

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -5,11 +5,13 @@ class ReviewController < ApplicationController
   end
 
   def results
+    @datasift = DataSiftQueryRunner.new
     @term = params[:review][:term] || ""
     render 'review/search'
   end
 
   def dummy
+    @datasift = DataSiftQueryRunner.new
     @term = params[:term] || ""
     render 'review/dummy'
   end
