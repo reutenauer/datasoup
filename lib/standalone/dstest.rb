c68b94353d24838c1c80bbc500a875ed2ca2c749
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-05 15:14:23 +0100

    Working on dstest

--- lib/standalone/dstest.rb
+++ lib/standalone/dstest.rb
@@ -4,7 +4,8 @@ require 'yaml'
 require 'datasift'
 
 class DataSiftQueryRunner
-  def initialize
+  def initialize(results = 3)
+    @results = results
     dsconfig = YAML::load(File.read(File.join(Rails.root, 'config', 'datasift.yml')))
     @username = dsconfig['username']
     @apikey = dsconfig['apikey']
@@ -22,15 +23,15 @@ class DataSiftQueryRunner
     consumer = definition.getConsumer(DataSift::StreamConsumer::TYPE_HTTP)
     n = 0
     consumer.consume(true) do |interaction|
-      stop if n == 3
+      stop if n == @results
 
       if interaction
-        puts interaction['interaction']['content']
+        sentiment = interaction['salience']['content']['sentiment']
+        puts "Sentiment = #{sentiment}: interaction['interaction']['content']"
         puts ''
         @output.puts(interaction.inspect)
+        n += 1 if sentiment != 0
       end
-
-      n += 1
     end
   end
 end
