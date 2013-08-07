47ffe7616702613180b0fc71eb7ba44f9f5fd562
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-07 01:09:26 +0100

    Major reorganisation of the back-end code, work in progress.

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -3,29 +3,25 @@ include DataSiftHelper
 
 class ReviewController < ApplicationController
   def search
-    @datasift = DataSiftQueryRunner.new
     render 'review/search'
   end
 
   def results
-    @datasift = DataSiftQueryRunner.new
     @term = params[:review][:term] || ""
     render 'review/search'
   end
 
   def search_job_id
-    @datasift = DataSiftQueryRunner.new
     @term = params[:term] || ""
     # @hits = @datasift.start("twitter.text contains \"#{@term}\"") # TODO escape!
     # @id = @datasift.dummy
-    @id = @datasift.start("twitter.text contains \"#{@term}\"")
+    @id = DataSiftHelper.search(@term)
     render 'review/search_job_id'
   end
 
   def search_hits
-    @datasift = DataSiftQueryRunner.new
     @id = params[:id]
-    @hits = @datasift.hits(@id)
+    @hits = DataSiftHelper.hits(@id)
     puts "Controller: got #{@hits.count} hits so far."
     render 'review/search_hits'
   end
--- lib/datasift_query_runner.rb
+++ lib/datasift_query_runner.rb
@@ -5,39 +5,40 @@ require 'datasift'
 require 'digest'
 require 'redis'
 
-class DataSiftQueryRunner
+module DataSiftHelper
   class StopConsuming < Exception
   end
 
-  attr_reader :results, :user, :output
+  # @datasift_config = File.join(Rails.root, 'config', 'datasift.yml')
+  @datasift_config = File.expand_path('../../config/datasift.yml', __FILE__)
+  if ENV['DATASIFT_APIKEY']
+    # FIXME Why we need that is incomprehensible.  Might be a problem
+    # with the DataSift library below.
+    @username = ENV['DATASIFT_USERNAME'].each_byte.inject("") { |s, b| s + b.chr }
+    @apikey = ENV['DATASIFT_APIKEY'].each_byte.inject("") { |s, b| s + b.chr }
+  elsif File.file?(@datasift_config)
+    dsconfig = YAML::load(File.read(@datasift_config))
+    @username = dsconfig['username']
+    @apikey = dsconfig['apikey']
+  else
+    raise
+  end
+  @user = DataSift::User.new(@username, @apikey)
 
-  def initialize(results = 3)
-    @results = results
-    datasift_config = File.join(Rails.root, 'config', 'datasift.yml')
-    if ENV['DATASIFT_APIKEY']
-      # FIXME Why we need that is incomprehensible.  Might be a problem
-      # with the DataSift library below.
-      @username = ENV['DATASIFT_USERNAME'].each_byte.inject("") { |s, b| s + b.chr }
-      @apikey = ENV['DATASIFT_APIKEY'].each_byte.inject("") { |s, b| s + b.chr }
-    elsif File.file?(datasift_config)
-      dsconfig = YAML::load(File.read(datasift_config))
-      @username = dsconfig['username']
-      @apikey = dsconfig['apikey']
-    else
-      raise
-    end
-    @user = DataSift::User.new(@username, @apikey)
-    @output = File.open(File.join(Rails.root, 'public', 'datasoup.txt'), 'a')
+  def self.user
+    @user
   end
 
-  def stop
-    # TODO Stop on time
+  def stopsifting
+    # TODO Allow stopping on time too
     @output.close
     raise StopConsuming
   end
 
-  def start(query)
-    unique_id = Digest::SHA1::hexdigest("datasift_query:#{query}:#{Time.now.to_i}")
+  def self.search(search_term)
+    unique_id = Digest::SHA1::hexdigest("datasift_query:#{search_term}:#{Time.now.to_i}")
+    search_term.gsub(/"/, '\"') # TODO Improve
+    query = "twitter.text contains \"#{search_term}\""
     Resque.enqueue(DataSiftQueryJob, unique_id, query) # FIXME Blah
     unique_id
   end
@@ -48,7 +49,7 @@ class DataSiftQueryRunner
     unique_id
   end
 
-  def hits(unique_id)
+  def self.hits(unique_id)
     score_list = "datasoup:#{unique_id}:score"
     content_list = "datasoup:#{unique_id}:content"
     klout_list = "datasoup:#{unique_id}:klout"
@@ -58,8 +59,13 @@ class DataSiftQueryRunner
     end
   end
 
-  def balance
+  def self.balance
     @user.getBalance['credit']
+  end
+
+  def sentiment_to_grade(score_array)
+    grade = ((score_array.average + 20) / 4).round(1)
+    "#{grade} / 10"
   end
 end
 
@@ -80,19 +86,20 @@ end
 class DataSiftQueryJob
   @queue = :datasift_query
   include Resque::Plugins::UniqueJob
+  include DataSiftHelper
 
   def self.perform(unique_id, query)
     puts "DataSiftQuery worker called for query “#{query}” with ID #{unique_id}"
-    qr = DataSiftQueryRunner.new
-    nb_results = qr.results
-    user = qr.user
-    output = qr.output
+    nb_results = 3
+    output = File.open(File.join(Rails.root, 'public', 'datasoup.txt'), 'a')
+
+    user = DataSiftHelper.user
     definition = user.createDefinition(query)
     consumer = definition.getConsumer(DataSift::StreamConsumer::TYPE_HTTP)
     n = 0
     begin
       consumer.consume(true) do |interaction|
-        stop if n == nb_results
+        stopsifting if n == nb_results
 
         if interaction
           if interaction['salience']
@@ -114,15 +121,8 @@ class DataSiftQueryJob
           n += 1 if sentiment != nil and sentiment != 0
         end
       end
-    rescue DataSiftQueryRunner::StopConsuming
+    rescue StopConsuming
     end
 
-  end
-end
-
-module DataSiftHelper
-  def sentiment_to_grade(score_array)
-    grade = ((score_array.average + 20) / 4).round(1)
-    "#{grade} / 10"
   end
 end
